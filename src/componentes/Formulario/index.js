92a1abd06de4137f8ac6ce6cf95baef83e6f4ffe
evellynreis <dev.evellynreis@gmail.com>
2025-01-04 23:16:37 -0300

    Adicionando o componente de Lista Suspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,6 +1,17 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 function Formulario() {
+  const times = [
+    'Programação',
+    'Front-End',
+    'Data Science',
+    'Devops',
+    'UX e Design',
+    'Mobile',
+    'Inovação e Gestão',
+  ]
+
   return (
     <section className="formulario">
       <form>
@@ -8,6 +19,7 @@ function Formulario() {
         <CampoTexto label="Nome" placeholder="Digite o seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times}/>
       </form>
     </section>
   );
