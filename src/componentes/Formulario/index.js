e969a0e8ddec677a14f92c274f4f7750462b111c
evellynreis <dev.evellynreis@gmail.com>
2025-01-05 20:26:41 -0300

    Adicionando componente de Botao

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,16 +1,17 @@
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 function Formulario() {
   const times = [
-    'Programação',
-    'Front-End',
-    'Data Science',
-    'Devops',
-    'UX e Design',
-    'Mobile',
-    'Inovação e Gestão',
-  ]
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
 
   return (
     <section className="formulario">
@@ -19,7 +20,8 @@ function Formulario() {
         <CampoTexto label="Nome" placeholder="Digite o seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa label="Time" itens={times}/>
+        <ListaSuspensa label="Time" itens={times} />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
